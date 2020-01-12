3e93a0e5d0de5b798afb9e2ccdf59a92f3e6da27
Ranjithkumar-M <ranvitranjit@gmail.com>
2020-01-12 12:24:52 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TSQlik, QSApp, QSProps } from 'ts-qlik'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'qlik-angilar';
+export class AppComponent implements OnInit {
+
+  qlik = null;
+  active = 'qlik';
+  config = {
+    "host": "localhost",
+    "port": 80,
+    "prefix": "/",
+    "isSecure": false
+  };
+
+  ngOnInit() {
+    this.qlik = null;
+  }
+
+  callQlikFn = () => {
+    TSQlik(this.config).then((q) => {
+      this.qlik = q;
+    })
+  }
+
+  changeTab = (data) => {
+    this.active = data;
+    if(data === 'app') {
+      QSApp('031ec68a-9247-4fc5-9ddd-3b1764924aab', this.config).then((q) => {
+        q.currApp.app.getObject('obj1', 'VtfsZDc')
+        q.currApp.app.getObject('obj2', 'JcJvj')
+      })
+    }
+  }
+
 }
